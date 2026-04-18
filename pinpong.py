e2f6976e047e293b8aa63545a16e199ef0d6593b
Chon4ik <starikoff.sergey.09@gmail.com>
2026-04-18 18:12:08 +0500

    Update pinpong.py to full version

--- pinpong.py
+++ pinpong.py
@@ -11,11 +11,17 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image,(self.rect.x,self.rect.y))
 class Player(GameSprite):
-    def update(self):
+    def update1(self):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed 
         if keys[K_s] and self.rect.y <win_height-100:
+            self.rect.y += self.speed
+    def update2(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed 
+        if keys[K_DOWN] and self.rect.y <win_height-100:
             self.rect.y += self.speed
 class Bullet(GameSprite):
     def update(self):
@@ -29,20 +35,17 @@ win_height = 500
 speed1 = 10
 x1 = 50
 y1= 250
+speed_y = 6
 speed2 = 6
 display.set_caption('Пинпонг')
 player1=Player("player.png",x1,y1,speed1,40,100)
 player2=Player("player.png",(x1+570),y1,speed1,40,100)
-ball = Bullet("ball.png",350,250,speed1,40,40)
-boss1 = sprite.Group()
-boss1.add(ball)
-players = sprite.Group()
-players.add(player1)
-players.add(player2)
+ball = Bullet("ball.png",350,250,speed2,40,40)
 background = transform.scale(image.load('galaxy.jpg'),(700,500))
 clock = time.Clock()
 FPS = 60
-text_win = font1.render('you win',True,(255,255,55))
+total = 0
+text_lose = font1.render('you all lose',True,(255,255,55))
 finish = False
 game = True
 while game:
@@ -52,11 +55,24 @@ while game:
     if finish!=True:
         window.blit(background,(0,0))
         ball.reset()
-        ball.update()
         player1.reset()
-        player1.update()
+        player1.update1()
         player2.reset()
-        player2.update()
-        sprites_list2 = sprite.groupcollide(players,boss1,True,True)
+        player2.update2()
+        ball.rect.x += speed2
+        ball.rect.y += speed_y
+        if ball.rect.y > win_height - 50 or ball.rect.y < 0:
+            speed_y*= -1
+        sprites_list1 = sprite.collide_rect(player1,ball)
+        sprites_list2 = sprite.collide_rect(player2,ball)
+        if sprites_list2 or sprites_list1:
+            speed2 *= -1
+        if ball.rect.x > 680 or ball.rect.x < 20:
+            total += 1
+            ball.rect.x = 350
+            ball.rect.y = 250
+        if total == 3:
+            window.blit(text_lose,(200,200))
+            finish = True
     display.update()
     clock.tick(FPS)
